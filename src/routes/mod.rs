e01398d70e418204407fae07ca059619a728ffad
adsick <vadimgangsta73@gmail.com>
2021-06-07 20:01:02 +0300

    upload (not sure about get route) postvote with access

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,16 +1,16 @@
 mod comment;
 mod global;
 mod query;
+mod upload;
 mod user;
 mod vote;
-mod upload;
 
 pub use comment::*;
 pub use global::*;
 pub use query::*;
+pub use upload::*;
 pub use user::*;
 pub use vote::*;
-pub use upload::*;
 
 mod utils {
     pub use crate::data::*;
